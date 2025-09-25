3d88e0f03fd0e183a378fed165ceb9d1601c0974
KUNALI BHAVARTHE <kunalibhavarthe2003@gmail.com>
2025-09-25 22:30:44 +0530

    Update sub_search.c

--- sub_search.c
+++ sub_search.c
@@ -4,7 +4,7 @@ int n,i,key,found=0;
 printf("enter number of elements:");
 scanf("%d",&n);
 int a[n];
-printf("enter %d elements:\n");
+printf("enter %d elements:\n",n);
 for(i=0;i<n;i++)
 {
     scanf("%d",&a[i]);
